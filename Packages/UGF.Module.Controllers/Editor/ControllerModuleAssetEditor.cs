8f4a4858bdbdd1702d5b336b9b4902e5b9b19cb7
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-06 22:10:03 +0300

    Add editors

--- Packages/UGF.Module.Controllers/Editor/ControllerModuleAssetEditor.cs
+++ Packages/UGF.Module.Controllers/Editor/ControllerModuleAssetEditor.cs
@@ -1,6 +1,45 @@
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Controllers.Runtime;
+using UnityEditor;
+
 namespace UGF.Module.Controllers.Editor
 {
-    internal class ControllerModuleAssetEditor
+    [CustomEditor(typeof(ControllerModuleAsset), true)]
+    internal class ControllerModuleAssetEditor : UnityEditor.Editor
     {
+        private SerializedProperty m_propertyScript;
+        private SerializedProperty m_propertyUseReverseUninitializationOrder;
+        private ReorderableListDrawer m_listControllers;
+
+        private void OnEnable()
+        {
+            m_propertyScript = serializedObject.FindProperty("m_Script");
+            m_propertyUseReverseUninitializationOrder = serializedObject.FindProperty("m_useReverseUninitializationOrder");
+            m_listControllers = new AssetReferenceListDrawer(serializedObject.FindProperty("m_controllers"));
+
+            m_listControllers.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listControllers.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                using (new EditorGUI.DisabledScope(true))
+                {
+                    EditorGUILayout.PropertyField(m_propertyScript);
+                }
+
+                EditorGUILayout.PropertyField(m_propertyUseReverseUninitializationOrder);
+
+                m_listControllers.DrawGUILayout();
+            }
+        }
     }
 }
