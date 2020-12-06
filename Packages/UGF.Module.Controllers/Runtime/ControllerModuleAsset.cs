395b9b37209a07ee889ffd21535c4db3a5ed867f
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-06 22:09:52 +0300

    Update ControllerModuleAsset.cs

--- Packages/UGF.Module.Controllers/Runtime/ControllerModuleAsset.cs
+++ Packages/UGF.Module.Controllers/Runtime/ControllerModuleAsset.cs
@@ -1,6 +1,39 @@
+using System.Collections.Generic;
+using UGF.Application.Runtime;
+using UGF.EditorTools.Runtime.IMGUI.AssetReferences;
+using UnityEngine;
+
 namespace UGF.Module.Controllers.Runtime
 {
-    public class ControllerModuleAsset
+    [CreateAssetMenu(menuName = "Unity Game Framework/Controllers/Controller Module", order = 2000)]
+    public class ControllerModuleAsset : ApplicationModuleAsset<IControllerModule, ControllerModuleDescription>
     {
+        [SerializeField] private bool m_useReverseUninitializationOrder = true;
+        [SerializeField] private List<AssetReference<ControllerAsset>> m_controllers = new List<AssetReference<ControllerAsset>>();
+
+        public bool UseReverseUninitializationOrder { get { return m_useReverseUninitializationOrder; } set { m_useReverseUninitializationOrder = value; } }
+        public List<AssetReference<ControllerAsset>> Controllers { get { return m_controllers; } }
+
+        protected override IApplicationModuleDescription OnBuildDescription()
+        {
+            var description = new ControllerModuleDescription(typeof(IControllerModule))
+            {
+                UseReverseUninitializationOrder = m_useReverseUninitializationOrder
+            };
+
+            for (int i = 0; i < m_controllers.Count; i++)
+            {
+                AssetReference<ControllerAsset> reference = m_controllers[i];
+
+                description.Controllers.Add(reference.Guid, reference.Asset);
+            }
+
+            return description;
+        }
+
+        protected override IControllerModule OnBuild(ControllerModuleDescription description, IApplication application)
+        {
+            return new ControllerModule(description, application);
+        }
     }
 }
