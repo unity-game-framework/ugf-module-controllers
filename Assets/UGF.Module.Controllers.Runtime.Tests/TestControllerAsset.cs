7c29208ac49c4f178ec144cc208cda81cdab562f
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 19:25:27 +0300

    Add tests

---
+++ Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerAsset.cs
@@ -0,0 +1,22 @@
+﻿using UGF.Application.Runtime;
+using UGF.Initialize.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Controllers.Runtime.Tests
+{
+    [CreateAssetMenu(menuName = "Tests/TestControllerAsset")]
+    public class TestControllerAsset : ControllerAsset<TestController>
+    {
+        protected override TestController OnBuild(ControllerDescription description, IApplication application)
+        {
+            return new TestController(description, application);
+        }
+    }
+
+    public class TestController : Controller<ControllerDescription>
+    {
+        public TestController(ControllerDescription description, IApplication application) : base(description, application)
+        {
+        }
+    }
+}
--- Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerModule.cs
+++ Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerModule.cs
@@ -1,6 +1,38 @@
+using NUnit.Framework;
+using UGF.Application.Runtime;
+using UnityEngine;
+
 namespace UGF.Module.Controllers.Runtime.Tests
 {
     public class TestControllerModule
     {
+        [Test]
+        public void InitializeAndUninitialize()
+        {
+            IApplication application = CreateApplication();
+
+            application.Initialize();
+
+            var module = application.GetModule<IControllerModule>();
+
+            Assert.NotNull(module);
+            Assert.True(module.Controllers.ContainsKey("0cecb78aa3f74e24795a355a78af8962"));
+
+            application.Uninitialize();
+        }
+
+        private IApplication CreateApplication()
+        {
+            return new ApplicationConfigured(new ApplicationResources
+            {
+                new ApplicationConfig
+                {
+                    Modules =
+                    {
+                        (IApplicationModuleBuilder)Resources.Load("Module", typeof(IApplicationModuleBuilder))
+                    }
+                }
+            });
+        }
     }
 }
