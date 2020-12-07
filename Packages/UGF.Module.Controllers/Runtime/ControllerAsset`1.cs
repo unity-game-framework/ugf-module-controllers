68c2b96f8d0aed17357f3d5731fc087c10855671
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-07 19:25:23 +0300

    Update ControllerAsset`1.cs

--- Packages/UGF.Module.Controllers/Runtime/ControllerAsset`1.cs
+++ Packages/UGF.Module.Controllers/Runtime/ControllerAsset`1.cs
@@ -1,6 +1,6 @@
 ﻿namespace UGF.Module.Controllers.Runtime
 {
-    public abstract class ControllerAsset<TController> : ControllerAsset<TController, IControllerDescription> where TController : class, IController
+    public abstract class ControllerAsset<TController> : ControllerAsset<TController, ControllerDescription> where TController : class, IController
     {
         protected override IControllerDescription OnBuildDescription()
         {
