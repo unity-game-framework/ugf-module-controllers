a73308a86c12ea669a1a5b48e62cdd5b365bbce8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-08 20:00:59 +0300

    Add tests

---
+++ Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerComponent.cs
@@ -0,0 +1,12 @@
+﻿using UGF.Application.Runtime;
+
+namespace UGF.Module.Controllers.Runtime.Tests
+{
+    public class TestControllerComponent : ControllerComponent<TestController>
+    {
+        protected override TestController OnBuild(ControllerDescription description, IApplication application)
+        {
+            return new TestController(description, application);
+        }
+    }
+}
