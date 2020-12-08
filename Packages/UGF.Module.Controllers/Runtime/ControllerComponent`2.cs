d34ff575799ba7ce4f3130008b783eca7e83137d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-08 20:00:52 +0300

    Add ControllerComponent

---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerComponent`2.cs
@@ -0,0 +1,16 @@
+﻿using UGF.Application.Runtime;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    public abstract class ControllerComponent<TController, TDescription> : ControllerComponent
+        where TController : class, IController
+        where TDescription : class, IControllerDescription
+    {
+        protected override IController OnBuild(IApplication arguments, IControllerDescription description)
+        {
+            return OnBuild((TDescription)description, arguments);
+        }
+
+        protected abstract TController OnBuild(TDescription description, IApplication application);
+    }
+}
