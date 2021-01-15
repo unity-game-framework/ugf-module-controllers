5173bbf7ec044a784cf12962cd01e02351dc12d6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-15 21:29:42 +0300

    Add builders

---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerBuilder`2.cs
@@ -0,0 +1,23 @@
+﻿using UGF.Application.Runtime;
+using UGF.Builder.Runtime;
+using UGF.Description.Runtime;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    public abstract class ControllerBuilder<TController, TDescription> : DescribedWithDescriptionBuilder<IApplication, TController, TDescription>, IControllerBuilder
+        where TController : class, IController
+        where TDescription : class, IControllerDescription
+    {
+        protected override TController OnBuild(IApplication arguments, TDescription description)
+        {
+            return OnBuild(description, arguments);
+        }
+
+        protected abstract TController OnBuild(TDescription description, IApplication application);
+
+        IController IBuilder<IApplication, IController>.Build(IApplication arguments)
+        {
+            return Build(arguments);
+        }
+    }
+}
