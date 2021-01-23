4e2dfce4f2e0adf9df67fd45d42e3311f8d954f5
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-23 19:53:05 +0300

    Update ControllerBuilder.cs

--- Packages/UGF.Module.Controllers/Runtime/ControllerBuilder.cs
+++ Packages/UGF.Module.Controllers/Runtime/ControllerBuilder.cs
@@ -5,6 +5,11 @@ namespace UGF.Module.Controllers.Runtime
 {
     public abstract class ControllerBuilder<TController> : Builder<IApplication, TController>, IControllerBuilder where TController : class, IController
     {
+        T IBuilder<IApplication, IController>.Build<T>(IApplication arguments)
+        {
+            return (T)(object)Build(arguments);
+        }
+
         IController IBuilder<IApplication, IController>.Build(IApplication arguments)
         {
             return Build(arguments);
