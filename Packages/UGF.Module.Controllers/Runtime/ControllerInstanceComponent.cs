80c9b6b18c58168c03ed67e107184a554178055b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 17:10:55 +0300

    Change ControllerInstanceComponent

--- Packages/UGF.Module.Controllers/Runtime/ControllerInstanceComponent.cs
+++ Packages/UGF.Module.Controllers/Runtime/ControllerInstanceComponent.cs
@@ -1,17 +1,24 @@
 ﻿using System;
+using UGF.Application.Runtime;
 using UGF.EditorTools.Runtime.IMGUI.Attributes;
 using UnityEngine;
 
 namespace UGF.Module.Controllers.Runtime
 {
-    public abstract class ControllerInstanceComponent : MonoBehaviour
+    [AddComponentMenu("Unity Game Framework/Controllers/Controller Instance", 2000)]
+    public class ControllerInstanceComponent : ControllerComponent
     {
+        [SerializeField] private bool m_buildOnAwake = true;
+        [SerializeField] private bool m_buildUnique;
+        [AssetGuid(typeof(ControllerInstanceProviderControllerAsset))]
+        [SerializeField] private string m_provider;
         [AssetGuid(typeof(ControllerAsset))]
         [SerializeField] private string m_controller;
-        [SerializeField] private bool m_buildUnique;
 
-        public string Controller { get { return m_controller; } set { m_controller = value; } }
+        public bool BuildOnAwake { get { return m_buildOnAwake; } set { m_buildOnAwake = value; } }
         public bool BuildUnique { get { return m_buildUnique; } set { m_buildUnique = value; } }
+        public string Provider { get { return m_provider; } set { m_provider = value; } }
+        public string Controller { get { return m_controller; } set { m_controller = value; } }
         public string InstanceId { get { return !string.IsNullOrEmpty(m_instanceId) ? m_instanceId : throw new ArgumentException("Value not specified."); } }
         public IController Instance { get { return m_instance ?? throw new ArgumentException("Value not specified."); } }
         public bool HasInstance { get { return m_instance != null; } }
@@ -19,13 +26,22 @@ namespace UGF.Module.Controllers.Runtime
         private string m_instanceId;
         private IController m_instance;
 
-        protected abstract IController OnBuild();
+        protected override IController OnBuild(IApplication application)
+        {
+            var provider = application.GetController<IControllerInstanceProviderController>(m_provider);
+            IController controller = provider.Build(m_controller);
+
+            return controller;
+        }
 
         private void Awake()
         {
-            m_instanceId = m_buildUnique ? Guid.NewGuid().ToString("N") : m_controller;
-            m_instance = OnBuild();
-            m_instance.Application.AddController(m_instanceId, m_instance);
+            if (m_buildOnAwake && gameObject.TryGetApplication(out IApplication application))
+            {
+                m_instanceId = m_buildUnique ? Guid.NewGuid().ToString("N") : m_controller;
+                m_instance = OnBuild(application);
+                m_instance.Application.AddController(m_instanceId, m_instance);
+            }
         }
 
         private void Start()
