b4beef2b5053b49a7f52c327f3266659c225f198
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 16:18:44 +0300

    Add TestControllerApplicationExtensions

---
+++ Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerApplicationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using UGF.Application.Runtime;
+using UnityEngine;
+
+namespace UGF.Module.Controllers.Runtime.Tests
+{
+    public class TestControllerApplicationExtensions
+    {
+        [Test]
+        public void InitializeAndUninitialize()
+        {
+            IApplication application = CreateApplication();
+
+            application.Initialize();
+
+            var controller = application.GetController<TestController>();
+
+            Assert.NotNull(controller);
+
+            application.Uninitialize();
+        }
+
+        private IApplication CreateApplication()
+        {
+            return new ApplicationConfigured(new ApplicationResources
+            {
+                new ApplicationConfig
+                {
+                    Modules =
+                    {
+                        (IApplicationModuleBuilder)Resources.Load("Module", typeof(IApplicationModuleBuilder))
+                    }
+                }
+            });
+        }
+    }
+}
