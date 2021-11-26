db3de59d5bdc36a926bca1de0c79b16867964950
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 17:03:33 +0300

    Add ControllerInstanceProviderControllerAssetEditor

---
+++ Packages/UGF.Module.Controllers/Editor/ControllerInstanceProviderControllerAssetEditor.cs
@@ -0,0 +1,67 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Controllers.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Controllers.Editor
+{
+    [CustomEditor(typeof(ControllerInstanceProviderControllerAsset), true)]
+    internal class ControllerInstanceProviderControllerAssetEditor : UnityEditor.Editor
+    {
+        private AssetReferenceListDrawer m_listControllers;
+        private ReorderableListSelectionDrawerByPath m_listControllersSelection;
+        private ReorderableListDrawer m_listCollections;
+        private ReorderableListSelectionDrawerByElement m_listCollectionsSelection;
+
+        private void OnEnable()
+        {
+            m_listControllers = new AssetReferenceListDrawer(serializedObject.FindProperty("m_controllers"));
+
+            m_listControllersSelection = new ReorderableListSelectionDrawerByPath(m_listControllers, "m_asset")
+            {
+                Drawer =
+                {
+                    DisplayTitlebar = true
+                }
+            };
+
+            m_listCollections = new ReorderableListDrawer(serializedObject.FindProperty("m_collections"));
+
+            m_listCollectionsSelection = new ReorderableListSelectionDrawerByElement(m_listCollections)
+            {
+                Drawer =
+                {
+                    DisplayTitlebar = true
+                }
+            };
+
+            m_listControllers.Enable();
+            m_listControllersSelection.Enable();
+            m_listCollections.Enable();
+            m_listCollectionsSelection.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listControllers.Disable();
+            m_listControllersSelection.Disable();
+            m_listCollections.Disable();
+            m_listControllersSelection.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listControllers.DrawGUILayout();
+                m_listCollections.DrawGUILayout();
+
+                m_listControllersSelection.DrawGUILayout();
+                m_listCollectionsSelection.DrawGUILayout();
+            }
+        }
+    }
+}
