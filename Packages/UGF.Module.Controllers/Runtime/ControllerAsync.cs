d1dab585bd0b55f7a424ca7c6628eb7f6fd90430
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-26 16:23:30 +0300

    Add ControllerAsync and ControllerDescribedAsync

---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerAsync.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using UGF.Application.Runtime;
+using UGF.Initialize.Runtime;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    public abstract class ControllerAsync : ControllerBase, IControllerAsyncInitialize
+    {
+        private InitializeState m_state;
+
+        protected ControllerAsync(IApplication application) : base(application)
+        {
+        }
+
+        protected ControllerAsync(IApplication application, IInitializeCollection children) : base(application, children)
+        {
+        }
+
+        protected override void OnUninitialize()
+        {
+            base.OnUninitialize();
+
+            m_state = m_state.Uninitialize();
+        }
+
+        public Task InitializeAsync()
+        {
+            m_state = m_state.Initialize();
+
+            return OnInitializeAsync();
+        }
+
+        protected virtual Task OnInitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
