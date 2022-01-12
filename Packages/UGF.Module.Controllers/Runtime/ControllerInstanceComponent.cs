8d9e72549455374a4aca20c28607f5d07838f2f7
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-01-12 19:52:42 +0300

    Update ControllerInstanceComponent.cs

--- Packages/UGF.Module.Controllers/Runtime/ControllerInstanceComponent.cs
+++ Packages/UGF.Module.Controllers/Runtime/ControllerInstanceComponent.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using UGF.Application.Runtime;
 using UGF.EditorTools.Runtime.IMGUI.Attributes;
 using UGF.Module.Controllers.Runtime.Objects;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UGF.Module.Controllers.Runtime
 {
@@ -18,6 +20,7 @@ namespace UGF.Module.Controllers.Runtime
         [SerializeField] private bool m_relativeToComponent;
         [AssetGuid(typeof(ObjectRelativesControllerAsset))]
         [SerializeField] private string m_relativesProvider;
+        [SerializeField] private List<Object> m_relatives = new List<Object>();
 
         public bool BuildOnAwake { get { return m_buildOnAwake; } set { m_buildOnAwake = value; } }
         public bool BuildAsSingleton { get { return m_buildAsSingleton; } set { m_buildAsSingleton = value; } }
@@ -25,6 +28,7 @@ namespace UGF.Module.Controllers.Runtime
         public string Controller { get { return m_controller; } set { m_controller = value; } }
         public bool RelativeToComponent { get { return m_relativeToComponent; } set { m_relativeToComponent = value; } }
         public string RelativesProvider { get { return m_relativesProvider; } set { m_relativesProvider = value; } }
+        public List<Object> Relatives { get { return m_relatives; } }
         public string InstanceId { get { return !string.IsNullOrEmpty(m_instanceId) ? m_instanceId : throw new ArgumentException("Value not specified."); } }
         public IController Instance { get { return m_instance ?? throw new ArgumentException("Value not specified."); } }
         public bool HasInstance { get { return m_instance != null; } }
@@ -50,7 +54,14 @@ namespace UGF.Module.Controllers.Runtime
 
                 if (m_relativeToComponent)
                 {
-                    m_instance.Application.GetController<IObjectRelativesController>(m_relativesProvider).Provider.Connect(m_instance, this);
+                    var objectRelativesController = m_instance.Application.GetController<IObjectRelativesController>(m_relativesProvider);
+
+                    objectRelativesController.Provider.Connect(m_instance, this);
+
+                    for (int i = 0; i < m_relatives.Count; i++)
+                    {
+                        objectRelativesController.Provider.Connect(m_instance, m_relatives[i]);
+                    }
                 }
             }
         }
@@ -68,7 +79,14 @@ namespace UGF.Module.Controllers.Runtime
 
                 if (m_relativeToComponent)
                 {
-                    m_instance.Application.GetController<IObjectRelativesController>(m_relativesProvider).Provider.Disconnect(m_instance, this);
+                    var objectRelativesController = m_instance.Application.GetController<IObjectRelativesController>(m_relativesProvider);
+
+                    objectRelativesController.Provider.Disconnect(m_instance, this);
+
+                    for (int i = 0; i < m_relatives.Count; i++)
+                    {
+                        objectRelativesController.Provider.Disconnect(m_instance, m_relatives[i]);
+                    }
                 }
 
                 m_instance.Application.RemoveController(m_instanceId);
