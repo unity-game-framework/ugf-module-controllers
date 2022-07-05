062c8c927bc4dd21b8b9c83fd84a7330ea9af7d6
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-05 17:47:02 +0300

    Add ControllerCollectionController

---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerCollectionController.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using UGF.Application.Runtime;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    public class ControllerCollectionController : ControllerDescribedAsync<ControllerCollectionControllerDescription>
+    {
+        public ControllerCollection<IController> Controllers { get; } = new ControllerCollection<IController>();
+
+        public ControllerCollectionController(ControllerCollectionControllerDescription description, IApplication application) : base(description, application)
+        {
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            foreach ((string key, IControllerBuilder value) in Description.Controllers)
+            {
+                IController controller = value.Build(Application);
+
+                Controllers.Add(key, controller);
+                Application.AddController(key, controller);
+            }
+
+            Controllers.Initialize();
+        }
+
+        protected override async Task OnInitializeAsync()
+        {
+            await base.OnInitializeAsync();
+
+            foreach ((_, IController value) in Controllers)
+            {
+                if (value is IControllerAsyncInitialize controller)
+                {
+                    await controller.InitializeAsync();
+                }
+            }
+        }
+
+        protected override void OnUninitialize()
+        {
+            base.OnUninitialize();
+
+            Controllers.Uninitialize();
+
+            foreach ((string key, _) in Controllers)
+            {
+                Application.RemoveController(key);
+            }
+
+            Controllers.Clear();
+        }
+    }
+}
---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerCollectionControllerAsset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UGF.Application.Runtime;
+using UGF.EditorTools.Runtime.IMGUI.AssetReferences;
+using UnityEngine;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Controllers/Controller Collection Controller", order = 2000)]
+    public class ControllerCollectionControllerAsset : ControllerDescribedAsset<ControllerCollectionController, ControllerCollectionControllerDescription>
+    {
+        [SerializeField] private List<AssetReference<ControllerAsset>> m_controllers = new List<AssetReference<ControllerAsset>>();
+
+        public List<AssetReference<ControllerAsset>> Controllers { get { return m_controllers; } set { m_controllers = value; } }
+
+        protected override ControllerCollectionControllerDescription OnBuildDescription()
+        {
+            var description = new ControllerCollectionControllerDescription();
+
+            for (int i = 0; i < m_controllers.Count; i++)
+            {
+                AssetReference<ControllerAsset> reference = m_controllers[i];
+
+                description.Controllers.Add(reference.Guid, reference.Asset);
+            }
+
+            return description;
+        }
+
+        protected override ControllerCollectionController OnBuild(ControllerCollectionControllerDescription description, IApplication application)
+        {
+            return new ControllerCollectionController(description, application);
+        }
+    }
+}
