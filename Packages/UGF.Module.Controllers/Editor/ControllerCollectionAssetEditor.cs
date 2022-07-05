c693628e928c093a2e694caead2904e2b695c429
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-05 17:19:03 +0300

    Change ControllerCollectionAssetEditor

--- Packages/UGF.Module.Controllers/Editor/ControllerCollectionAssetEditor.cs
+++ Packages/UGF.Module.Controllers/Editor/ControllerCollectionAssetEditor.cs
@@ -1,20 +1,20 @@
 ﻿using UGF.EditorTools.Editor.IMGUI;
-using UGF.EditorTools.Editor.IMGUI.AssetReferences;
 using UGF.EditorTools.Editor.IMGUI.Scopes;
 using UGF.Module.Controllers.Runtime;
 using UnityEditor;
+using UnityEngine;
 
 namespace UGF.Module.Controllers.Editor
 {
     [CustomEditor(typeof(ControllerCollectionAsset), true)]
     internal class ControllerCollectionAssetEditor : UnityEditor.Editor
     {
-        private AssetReferenceListDrawer m_listControllers;
+        private ControllerCollectionAssetListDrawer m_listControllers;
         private ReorderableListSelectionDrawerByPath m_listControllersSelection;
 
         private void OnEnable()
         {
-            m_listControllers = new AssetReferenceListDrawer(serializedObject.FindProperty("m_controllers"));
+            m_listControllers = new ControllerCollectionAssetListDrawer(serializedObject.FindProperty("m_controllers"));
 
             m_listControllersSelection = new ReorderableListSelectionDrawerByPath(m_listControllers, "m_asset")
             {
@@ -30,8 +30,8 @@ namespace UGF.Module.Controllers.Editor
 
         private void OnDisable()
         {
-            m_listControllers.Disable();
             m_listControllersSelection.Disable();
+            m_listControllers.Disable();
         }
 
         public override void OnInspectorGUI()
@@ -42,6 +42,15 @@ namespace UGF.Module.Controllers.Editor
 
                 m_listControllers.DrawGUILayout();
                 m_listControllersSelection.DrawGUILayout();
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                m_listControllers.DisplayAsReplace = GUILayout.Toggle(m_listControllers.DisplayAsReplace, "Replace", EditorStyles.miniButton);
             }
         }
     }
---
+++ Packages/UGF.Module.Controllers/Editor/ControllerCollectionAssetListDrawer.cs
@@ -0,0 +1,39 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.EditorTools.Editor.IMGUI.Attributes;
+using UGF.Module.Controllers.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Controllers.Editor
+{
+    internal class ControllerCollectionAssetListDrawer : ReorderableListKeyAndValueDrawer
+    {
+        public bool DisplayAsReplace { get; set; }
+
+        public ControllerCollectionAssetListDrawer(SerializedProperty serializedProperty) : base(serializedProperty, "m_guid", "m_asset")
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty propertyKey = serializedProperty.FindPropertyRelative(PropertyKeyName);
+            SerializedProperty propertyValue = serializedProperty.FindPropertyRelative(PropertyValueName);
+            string guid = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(propertyValue.objectReferenceValue));
+
+            if (propertyKey.stringValue != guid || DisplayAsReplace)
+            {
+                base.OnDrawElementContent(position, serializedProperty, index, isActive, isFocused);
+            }
+            else
+            {
+                AssetReferenceEditorGUIUtility.AssetReference(position, GUIContent.none, serializedProperty);
+            }
+        }
+
+        protected override void OnDrawKey(Rect position, SerializedProperty serializedProperty)
+        {
+            AttributeEditorGUIUtility.DrawAssetGuidField(position, serializedProperty, GUIContent.none, typeof(ControllerAsset));
+        }
+    }
+}
