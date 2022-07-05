c73a1a0fe9b702d93ab50a6d8d02f911cf9865aa
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-05 17:47:13 +0300

    Add editors

---
+++ Packages/UGF.Module.Controllers/Editor/ControllerCollectionControllerAssetEditor.cs
@@ -0,0 +1,48 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Controllers.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Controllers.Editor
+{
+    [CustomEditor(typeof(ControllerCollectionControllerAsset), true)]
+    internal class ControllerCollectionControllerAssetEditor : UnityEditor.Editor
+    {
+        private AssetReferenceListDrawer m_listControllers;
+        private ReorderableListSelectionDrawerByPath m_listControllersSelection;
+
+        private void OnEnable()
+        {
+            m_listControllers = new AssetReferenceListDrawer(serializedObject.FindProperty("m_controllers"));
+
+            m_listControllersSelection = new ReorderableListSelectionDrawerByPath(m_listControllers, "m_asset")
+            {
+                Drawer =
+                {
+                    DisplayTitlebar = true
+                }
+            };
+
+            m_listControllers.Enable();
+            m_listControllersSelection.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listControllersSelection.Disable();
+            m_listControllers.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listControllers.DrawGUILayout();
+                m_listControllersSelection.DrawGUILayout();
+            }
+        }
+    }
+}
---
+++ Packages/UGF.Module.Controllers/Editor/ControllerCollectionControllerComponentEditor.cs
@@ -0,0 +1,89 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.Module.Controllers.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Controllers.Editor
+{
+    [CustomEditor(typeof(ControllerCollectionControllerComponent), true)]
+    internal class ControllerCollectionControllerComponentEditor : UnityEditor.Editor
+    {
+        private ReorderableListDrawer m_listControllers;
+
+        private void OnEnable()
+        {
+            m_listControllers = new ReorderableListDrawer(serializedObject.FindProperty("m_controllers"));
+            m_listControllers.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listControllers.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                m_listControllers.DrawGUILayout();
+            }
+
+            EditorGUILayout.Space();
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.FlexibleSpace();
+
+                if (GUILayout.Button("Clear"))
+                {
+                    OnClear();
+                }
+
+                if (GUILayout.Button("Collect in Scene"))
+                {
+                    OnCollectInScene();
+                }
+
+                if (GUILayout.Button("Collect"))
+                {
+                    OnCollect();
+                }
+            }
+        }
+
+        private void OnCollect()
+        {
+            var component = (ControllerCollectionControllerComponent)target;
+
+            Undo.RegisterCompleteObjectUndo(target, "Controller Collection Collect");
+            EditorUtility.SetDirty(target);
+
+            ControllerCollectionUtility.GetComponents(component.Controllers, component);
+
+            serializedObject.Update();
+        }
+
+        private void OnCollectInScene()
+        {
+            var component = (ControllerCollectionControllerComponent)target;
+
+            Undo.RegisterCompleteObjectUndo(target, "Controller Collection Collect in Scene");
+            EditorUtility.SetDirty(target);
+
+            ControllerCollectionUtility.GetComponents(component.Controllers, component.gameObject.scene);
+
+            component.Controllers.Remove(component);
+
+            serializedObject.Update();
+        }
+
+        private void OnClear()
+        {
+            m_listControllers.SerializedProperty.ClearArray();
+            m_listControllers.SerializedProperty.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
