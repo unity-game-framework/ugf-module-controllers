0bcbb08f44b475a49625167d314286593acb7ab0
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-05 17:38:21 +0300

    Add ControllerUtility

---
+++ Packages/UGF.Module.Controllers/Runtime/ControllerUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Object = UnityEngine.Object;
+
+namespace UGF.Module.Controllers.Runtime
+{
+    public static class ControllerUtility
+    {
+        public static string GetId(Object target)
+        {
+            if (target == null) throw new ArgumentNullException(nameof(target));
+
+            uint id = (uint)target.GetInstanceID();
+
+            return id.ToString("x8");
+        }
+    }
+}
