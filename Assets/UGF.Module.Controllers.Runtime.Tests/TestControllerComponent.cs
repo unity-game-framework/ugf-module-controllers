d608294352b46e8e1add2c5bb0a013fc347fb006
Vorobey <vorobyovvorobyov@gmail.com>
2022-07-27 19:41:52 +0300

    Change tests

--- Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerComponent.cs
+++ Assets/UGF.Module.Controllers.Runtime.Tests/TestControllerComponent.cs
@@ -2,6 +2,7 @@
 
 namespace UGF.Module.Controllers.Runtime.Tests
 {
+    [ControllerComponentCollectPriority(-1)]
     public class TestControllerComponent : ControllerComponent
     {
         protected override IController OnBuild(IApplication arguments)
